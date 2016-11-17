e8eebea6b8d0f542a102be6bd06e9d6896b36322
Jason Browne <jason@browne.me.uk>
2016-11-17 22:23:35 +0000

    Add album list component and update routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { PaginationComponent }  from "./shared/pagination.component";
 import { MonthDetailComponent }   from "./calendar/month-detail.component";
 import { YearOverviewComponent }  from "./calendar/year-overview.component";
 import { AlbumFolderComponent }   from "./gallery/album-folder.component";
+import { AlbumListComponent }     from "./gallery/album-list.component";
 import { ImageViewComponent }     from "./gallery/image-view.component";
 import { AboutComponent }         from "./home/about.component";
 import { HomeComponent }          from "./home/home.component";
@@ -30,6 +31,7 @@ import { ProjectListComponent }   from "./projects/project-list.component";
   declarations: [
     AboutComponent,
     AlbumFolderComponent,
+    AlbumListComponent,
     AppComponent,
     ErrorComponent,
     HomeComponent,
