75af62e97dc8fd6ab30034105c2d76da23d830d3
Jason B <jaysunbee@gmail.com>
2017-02-18 21:08:31 +0000

    End to end test skeleton files

---
+++ e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, element, by } from 'protractor';
+
+export class Jblog3Page {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
