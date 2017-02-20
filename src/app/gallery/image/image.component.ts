6e8b9086c06b17f0fc4e8fab9854e8ad9210fbb0
Jason Browne <jason@browne.me.uk>
2017-02-20 23:02:23 +0000

    Port content for the image component

--- src/app/gallery/image/image.component.ts
+++ src/app/gallery/image/image.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from "@angular/router";
 
+import { ImageInfo } from '../image-info';
+
+import { ImageService } from '../image.service';
+
 @Component({
   selector: 'jblog-image',
   templateUrl: './image.component.html',
@@ -8,14 +12,39 @@ import { ActivatedRoute, Params } from "@angular/router";
 })
 export class ImageComponent implements OnInit {
 
-  private title: string;
+  private data: ImageInfo;
+  private isZoomedOut: boolean = true;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private imageService: ImageService) { }
 
   ngOnInit() {
     this.route.params.forEach((params: Params) => {
-      this.title = params["id"];
+      let imageId = +params["id"];
+      if (!imageId || imageId <= 0) {
+        return;
+      }
+
+      this.requestImageById(imageId);
     });
+  }
+
+  public toggleZoom(event: Event): void {
+    event.preventDefault();
+    this.isZoomedOut = !this.isZoomedOut;
+  }
+
+  private requestImageById(imageId: number): void {
+    this.data = null;
+
+    this.imageService.getImageInfo(imageId).subscribe(
+      res => this.handleImageResponse(res),
+      err => console.log('Error: %s', err),
+      () => console.log('Completed')
+    );
+  }
+
+  private handleImageResponse(response: ImageInfo): void {
+    this.data = response;
   }
 
 }
