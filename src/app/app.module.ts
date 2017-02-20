43927a718b229715857075cd215e2c8405720b2c
Jason B <jaysunbee@gmail.com>
2017-02-20 08:19:01 +0000

    Update app component and module to use gallery module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes }  from '@angular/router';
+
+import { GalleryModule } from './gallery/gallery.module';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    GalleryModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
