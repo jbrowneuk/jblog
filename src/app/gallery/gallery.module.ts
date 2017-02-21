2922bd4af7bc40c2ada6c4fd3542f6ba8e8e13b6
Jason Browne <jason@browne.me.uk>
2017-02-21 17:44:15 +0000

    Reformatting

--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+
 import { AlbumComponent } from './album/album.component';
 import { ImageComponent } from './image/image.component';
 import { AlbumListComponent } from './album-list/album-list.component';
 import { ThumbnailComponent } from './thumbnail/thumbnail.component';
-import { GalleryFormatPipe } from './gallery-format.pipe';
 
 import { ImageService } from './image.service';
+
 import { AlbumDescriptionPipe } from './album-description.pipe';
+import { GalleryFormatPipe } from './gallery-format.pipe';
 
 const galleryRoutes: Routes = [
   {path: 'art', component: AlbumComponent },
