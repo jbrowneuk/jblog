0b65d8216645448dce94d7c9541084492fdb1e03
Jason Browne <jason@browne.me.uk>
2017-02-21 18:30:20 +0000

    Add post and post-list component

--- src/app/journal/journal.module.ts
+++ src/app/journal/journal.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { PostComponent } from './post/post.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 const journalRoutes: Routes = [
-  {path: 'journal', component: null }
+  {path: 'journal', component: PostListComponent },
+  {path: 'journal/post/:id', component: PostComponent }
 ];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
     RouterModule.forChild(journalRoutes)
   ],
-  declarations: []
+  declarations: [PostComponent, PostListComponent]
 })
 export class JournalModule { }
---
+++ src/app/journal/post-list/post-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jblog-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.sass']
+})
+export class PostListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
