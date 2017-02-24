07c9a91db9212dbeebc84f3a245cd85163cdfee2
Jason Browne <jason@browne.me.uk>
2017-02-24 21:02:39 +0000

    Hook up post service to API

--- src/app/journal/post.service.ts
+++ src/app/journal/post.service.ts
@@ -8,21 +8,33 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
+const API_URL = '/api/?posts';
+
 @Injectable()
 export class PostService {
 
   constructor(private http: Http) { }
 
   public getPostsForPage(pageNumber: number): Observable<PostData[]> {
-    return this.http.get('/assets/mock-data/posts.json')
-      .map((response: Response) => response.json().data as PostData[])
-      .catch((error: any) => this.errorHandler(error));
+    let apiRequestUrl = API_URL;
+    if (pageNumber > 0) {
+      apiRequestUrl += `&page=${pageNumber}`;
+    }
+
+    return this.http.get(apiRequestUrl)
+      .catch((error: any) => this.errorHandler(error))
+      .map((response: Response) => response.json().data as PostData[]);
   }
 
   public getPost(id: number): Observable<PostData> {
-    return this.http.get('/assets/mock-data/post.json')
-      .map((response: Response) => response.json().data as PostData)
-      .catch((error: any) => this.errorHandler(error));
+    if (id <= 0) {
+      return this.errorHandler('Post does not exist');
+    }
+
+    const apiRequestUrl = `${API_URL}&postId=${id}`;
+    return this.http.get(apiRequestUrl)
+      .catch((error: any) => this.errorHandler(error))
+      .map((response: Response) => response.json().data as PostData);
   }
 
   private errorHandler(error: any): Observable<any> {
