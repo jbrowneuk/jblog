52d51c018f31cdaf334bb24371428f9739e429dd
Jason B <jaysunbee@gmail.com>
2017-02-27 13:32:04 +0000

    Move pagination component into current app

---
+++ src/app/shared/pagination/pagination.component.ts
@@ -0,0 +1,43 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'jblog-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.sass']
+})
+export class PaginationComponent {
+
+  @Input() public currentPage: number;
+  @Input() public totalPages: number;
+  @Input() public componentName: string;
+  @Input() public urlComponents: string[];
+  @Input() public isLoading = false;
+
+  public hasNextPage(): boolean {
+    return !this.isLoading &&
+           this.currentPage < this.totalPages &&
+           this.totalPages > 1;
+  }
+
+  public hasPreviousPage(): boolean {
+    return !this.isLoading &&
+           this.currentPage > 1 &&
+           this.totalPages > 1;
+  }
+
+  public getRouterLinkForPage(page: number): any[] {
+    let bits: any[] = [];
+    if (this.componentName) {
+      bits.push(`${this.componentName}`);
+    }
+
+    if (this.urlComponents) {
+      bits = ['/'].concat(this.urlComponents);
+    }
+
+    bits.push('page');
+    bits.push(page);
+    return bits;
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { LineSplittingPipe } from './line-splitting.pipe';
 
 import { TextParsingService } from './text-parsing.service';
+import { PaginationComponent } from './pagination/pagination.component';
 
 @NgModule({
-  declarations: [LineSplittingPipe],
+  declarations: [LineSplittingPipe, PaginationComponent],
   exports: [LineSplittingPipe]
 })
 export class SharedModule {
