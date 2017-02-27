e89a8a0fc4d51728f3d441b10aa602b70189c0b8
Jason Browne <jason@browne.me.uk>
2017-02-27 21:17:10 +0000

    Remove old components

--- old/home/art-insights.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'jblog-art-insights',
-  templateUrl: "./art-insights.component.html"
-})
-export class ArtInsightsComponent {
-}
--- old/journal/post-data-container.class.ts
+++
@@ -1,7 +0,0 @@
-import { PostData } from "./post-data.class";
-
-export class PostDataContainer {
-  currentPage: number;
-  totalPages: number;
-  data: PostData[];
-}
