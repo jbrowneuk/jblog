0cf6260c19efc2cc9803a4270e89c043b97fcc2e
Jason Browne <jason@browne.me.uk>
2017-02-28 19:57:23 +0000

    Remove unused OnInit from top page component

--- src/app/home/top-page/top-page.component.ts
+++ src/app/home/top-page/top-page.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'jblog-top-page',
   templateUrl: './top-page.component.html',
   styleUrls: ['./top-page.component.sass']
 })
-export class TopPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class TopPageComponent {
 }
