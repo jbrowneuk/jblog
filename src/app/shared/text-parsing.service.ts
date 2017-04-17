e7d58bbbbf8d2fcd05e559d205fd17ecaa62a7c0
Jason B <jaysunbee@gmail.com>
2017-04-17 22:16:50 +0100

    Add documentation for shared module items

--- src/app/shared/text-parsing.service.ts
+++ src/app/shared/text-parsing.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
 import { SafeHtml } from '@angular/platform-browser';
 
+/**
+ * A service used to parse text from its stored format into a form that can be
+ * rendered on a web page
+ */
 @Injectable()
 export class TextParsingService {
 
+  /**
+   * The emoji parsing engine, powered by emojiOne
+   */
   private emojiParser: any;
 
+  /**
+   * Constructor
+   */
   constructor() {
     this.emojiParser = require('emojione');
   }
 
+  /**
+   * Main text parsing method
+   */
   public parse(input: string): string {
     if (!input) {
       return '';
