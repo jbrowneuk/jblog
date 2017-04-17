a87e4efa0d7f7e50bb2ce8e09056c0a04a5b0d37
Jason B <jaysunbee@gmail.com>
2017-04-17 22:18:55 +0100

    Add missing documentation to error component

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 
+/**
+ * This component is used to surface an error to the user. It's mainly used for
+ * 404 (page not found) errors.
+ */
 @Component({
   selector: 'jblog-error',
   templateUrl: './error.component.html',
-  styleUrls: ['./error.component.sass', '../../shared-sass/content-info-area.sass']
+  styleUrls: [
+    './error.component.sass',
+    '../../shared-sass/content-info-area.sass'
+  ]
 })
 export class ErrorComponent {
 }
