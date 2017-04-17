4532f21f267d4dd6654c3c31bd626fdc0979862e
Jason Browne <jason@browne.me.uk>
2017-04-17 12:23:34 +0100

    Improve documentation for code module

--- src/app/code/project-detail/project-detail.component.ts
+++ src/app/code/project-detail/project-detail.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/**
+ * This component is used to show all the details about a specific code project,
+ * for example:
+ * - Programming languages
+ * - Supported platforms
+ * - Screen shots
+ */
 @Component({
   selector: 'jblog-project-detail',
   templateUrl: './project-detail.component.html',
   styleUrls: ['./project-detail.component.sass']
 })
-export class ProjectDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ProjectDetailComponent {
+  // TODO: implement this
 }
--- src/app/code/project.ts
+++ src/app/code/project.ts
@@ -1,8 +1,42 @@
+/**
+ * An interface which can be used by a class to encapsulate a Project.
+ */
 export interface Project {
+  /**
+   * The project's unique name
+   * @type {string}
+   */
   name: string;
+
+  /**
+   * The project's descriptive name - presented to the user
+   * @type {string}
+   */
   title: string;
+
+  /**
+   * The project's summary description text
+   * @type {string}
+   */
   summary: string;
+
+  /**
+   * The project's short information - usually the programming language and
+   * framework
+   * @type {string}
+   */
   info: string;
+
+  /**
+   * The project's external link - if set, used by the project list to link to
+   * a different location; otherwise the project detail component is used
+   * @type {string}
+   */
   link: string;
+
+  /**
+   * The project's resources (i.e. image) base path
+   * @type {string}
+   */
   resourcesUrl: string;
 }
