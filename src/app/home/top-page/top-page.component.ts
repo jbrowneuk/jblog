00cbd1e71d8d8def54a438e699d08fa859d720e0
Jason Browne <jason@browne.me.uk>
2017-05-17 21:36:40 +0100

    Remove dynamic projects list from top page
    The projects list updates far less frequently than the images, and judging by interest and word-of-mouth, visitors appear to be more interested in the artworks than the code projects.
    
    This change removes the code module from the top page, and instead replaces it with a simpler blurb and link to the code projects list.

--- src/app/home/top-page/top-page.component.ts
+++ src/app/home/top-page/top-page.component.ts
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'jblog-top-page',
   templateUrl: './top-page.component.html',
-  styleUrls: ['./top-page.component.scss']
+  styleUrls: [
+    './top-page.component.scss',
+    '../../../shared-sass/content-info-area.scss'
+  ]
 })
 export class TopPageComponent {
 }
