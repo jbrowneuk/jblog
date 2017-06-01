7bfd92b1158a2bc74ec9f619d9b1976cd3ff0b52
Jason Browne <jason@browne.me.uk>
2017-06-01 21:26:19 +0100

    Make settings template slightly more self-explanatory

--- src/api/conf/settings-template.php
+++ src/api/conf/settings-template.php
@@ -1,8 +1,22 @@
 <?php
 
+// Maximum number of posts to show on a journal page
+$postsVisibleOnPage = 5;
+
+// Whether in production mode or not
+$isProduction = false;
+
+// Calculate database file name and path from production switch
+$databaseFileName = $isProduction ? 'production' : 'debug';
+$databaseFilePath = './conf/' . $databaseFileName . '.sqlite';
+
+// Prefix for database tables
+$tablePrefix = 'blog_';
+
+// Aggregated settings array - required for running the backend
 $settings = array(
-  'Database' => array('Prefix' => 'blog_', 'Database' => './conf/production.sqlite'),
+  'Database' => array('Prefix' => $tablePrefix, 'Database' => $databaseFilePath),
   'Formats' => array('ShortDateTime' => 'd M Y, H:i', 'LongDateTime' => 'l, jS F Y, H:i T'),
-  'Defaults' => array('PostsVisible' => 5),
-  'Environment' => array('Production' => !file_exists('/Users/jbrowne/Sites/'))
+  'Defaults' => array('PostsVisible' => $postsVisibleOnPage),
+  'Environment' => array('Production' => $isProduction)
 );
