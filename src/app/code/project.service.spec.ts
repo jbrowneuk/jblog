848c7ea7deb61351ea5a791ccaac1cdb64b8195c
Jason Browne <jason@browne.me.uk>
2017-06-05 23:41:53 +0100

    Fix failing project service tests

--- src/app/code/project.service.spec.ts
+++ src/app/code/project.service.spec.ts
@@ -1,10 +1,100 @@
-import { TestBed, inject } from '@angular/core/testing';
-import { HttpModule, Http, BaseRequestOptions, XHRBackend } from '@angular/http';
+import { TestBed, async, inject } from '@angular/core/testing';
+import {
+  HttpModule,
+  Http,
+  Response,
+  ResponseOptions,
+  XHRBackend
+} from '@angular/http';
 import { MockBackend } from '@angular/http/testing';
+
+import { Project } from './project';
+import { ProjectService } from './project.service';
+
+describe('ProjectService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpModule],
+      providers: [
+        ProjectService,
+        { provide: XHRBackend, useClass: MockBackend },
+      ]
+    });
+  });
+
+  it('should get all projects from backend',
+    inject([ProjectService, XHRBackend], (service, mockBackend) => {
+      const mockResponse = {
+        data: [
+          {
+            name: 'test',
+            title: 'test project',
+            summary: 'test summary',
+            info: 'some info',
+            link: 'http://some.location/here',
+            resourcesUrl: 'http://some.location/'
+          },
+          {
+            name: 'test2',
+            title: 'test project2',
+            summary: 'test2 summary',
+            info: 'some info2',
+            link: 'http://some.location/here/2',
+            resourcesUrl: 'http://some.location/2'
+          }
+        ]
+      };
+
+      mockBackend.connections.subscribe((connection) => {
+        connection.mockRespond(new Response(new ResponseOptions({
+          body: JSON.stringify(mockResponse)
+        })));
+      });
+
+      service.getProjects(0).subscribe((projects: Project[]) => {
+        expect(projects.length).toBe(2);
+
+        expect(projects[0].name).toBe('test');
+        expect(projects[0].title).toBe('test project');
+        expect(projects[0].summary).toBe('test summary');
+        expect(projects[0].info).toBe('some info');
+        expect(projects[0].link).toBe('http://some.location/here');
+        expect(projects[0].resourcesUrl).toBe('http://some.location/');
+
+        expect(projects[1].name).toBe('test2');
+        expect(projects[1].title).toBe('test project2');
+        expect(projects[1].summary).toBe('test2 summary');
+        expect(projects[1].info).toBe('some info2');
+        expect(projects[1].link).toBe('http://some.location/here/2');
+        expect(projects[1].resourcesUrl).toBe('http://some.location/2');
+      });
+    })
+  );
+});
+
+/*import {
+  TestBed,
+  getTestBed,
+  async,
+  inject
+} from '@angular/core/testing';
+import {
+  BaseRequestOptions,
+  Response,
+  HttpModule,
+  Http,
+  XHRBackend,
+  RequestMethod
+} from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
+
+import { Project } from './project';
 
 import { ProjectService } from './project.service';
 
 describe('ProjectService', () => {
+  let mockBackend: MockBackend;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [ HttpModule ],
@@ -21,24 +111,39 @@ describe('ProjectService', () => {
         ProjectService
       ]
     });
+    mockBackend = getTestBed().get(MockBackend);
   });
 
-  it('should get all projects', inject([ProjectService], (service: ProjectService) => {
-    mockBackend.connections.subscribe((connection: MockConnection) => {
-      connection.mockRespond(new Response(
-        new ResponseOptions({
-          body: [
-            {
-              id: 26,
-              contentRendered: '<p><b>Hi there</b></p>',
-              contentMarkdown: '*Hi there*'
-            }]
-        }
-      )));
+  it('should not fuck up',
+    inject([ProjectService], (service: ProjectService) => {
+      //
     });
+  );
 
-    const projects = service.getProjects(0).subscribe(response => {
-      expect(response).not.toBeNull();
-    });
-  }));
-});
+  / *it('should get all projects asynchronously',
+    async(inject([ProjectService], (service: ProjectService) => {
+
+        mockBackend.connections.subscribe((connection: MockConnection) => {
+          const options = new ResponseOptions({
+            body: [{
+              name: 'test',
+              title: 'test project',
+              summary: 'test summary',
+              info: 'some info',
+              link: 'http://some.location/here',
+              resourcesUrl: 'http://some.location/'
+            }]
+          });
+          const response = new Response(options);
+          connection.mockRespond(response);
+        });
+
+        service.getProjects(0).subscribe((projects: Project[]) => {
+          expect(projects.length).toBe(1);
+          const data = projects[0];
+          expect(data.name).toBe('test');
+        });
+
+    }));
+  );* /
+});*/
