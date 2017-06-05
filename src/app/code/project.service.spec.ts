0a812806e0b32a0fe28c52b0c756ccebf221689d
Jason B <jaysunbee@gmail.com>
2017-06-05 15:59:38 +0100

    Add semi-working tests for some project components

--- src/app/code/project.service.spec.ts
+++ src/app/code/project.service.spec.ts
@@ -1,14 +1,44 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpModule, Http, BaseRequestOptions, XHRBackend } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
+
 import { ProjectService } from './project.service';
 
 describe('ProjectService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ProjectService]
+      imports: [ HttpModule ],
+      providers: [
+        MockBackend,
+        BaseRequestOptions,
+        {
+          provide: Http,
+          deps: [MockBackend, BaseRequestOptions],
+          useFactory: (backend: XHRBackend, defaultOptions: BaseRequestOptions) => {
+            return new Http(backend, defaultOptions);
+          }
+        },
+        ProjectService
+      ]
     });
   });
 
-  it('should ...', inject([ProjectService], (service: ProjectService) => {
-    expect(service).toBeTruthy();
+  it('should get all projects', inject([ProjectService], (service: ProjectService) => {
+    mockBackend.connections.subscribe((connection: MockConnection) => {
+      connection.mockRespond(new Response(
+        new ResponseOptions({
+          body: [
+            {
+              id: 26,
+              contentRendered: '<p><b>Hi there</b></p>',
+              contentMarkdown: '*Hi there*'
+            }]
+        }
+      )));
+    });
+
+    const projects = service.getProjects(0).subscribe(response => {
+      expect(response).not.toBeNull();
+    });
   }));
 });
--- src/app/code/projects-container/projects-container.component.spec.ts
+++ src/app/code/projects-container/projects-container.component.spec.ts
@@ -1,14 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { LineSplittingPipe } from '../../shared/line-splitting.pipe';
+import { ProjectService } from '../project.service';
+import { MockProjectService } from '../mock-project.service';
 
 import { ProjectsContainerComponent } from './projects-container.component';
 
 describe('ProjectsContainerComponent', () => {
+  const mockService = new MockProjectService();
   let component: ProjectsContainerComponent;
   let fixture: ComponentFixture<ProjectsContainerComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProjectsContainerComponent ]
+      imports: [ RouterTestingModule ],
+      declarations: [ LineSplittingPipe, ProjectsContainerComponent ],
+      providers: [{ provide: ProjectService, useValue: mockService }]
     })
     .compileComponents();
   }));
@@ -16,10 +25,38 @@ describe('ProjectsContainerComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProjectsContainerComponent);
     component = fixture.componentInstance;
+    component.projects = [{
+      'name': 'test',
+      'title': 'A test project',
+      'summary': 'Description of the test project',
+      'info': 'JSON data',
+      'link': 'https://www.google.com/',
+      'resourcesUrl': 'http://localhost:4200/assets/images/'
+    }];
+    compiled = fixture.debugElement.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display project title', () => {
+    expect(compiled.querySelector('h2').textContent).toContain('A test project');
+  });
+
+  it('should display project information', () => {
+    expect(compiled.querySelector('.content-info-area > ul > li').textContent)
+      .toContain('JSON data');
+  });
+
+  it('should display project information', () => {
+    expect(compiled.querySelector('.content-area > p').textContent)
+      .toContain('Description of the test project');
+  });
+
+  it('should display project link', () => {
+    expect(compiled.querySelector('.button.default').textContent)
+      .toContain('Go to project page');
   });
 });
