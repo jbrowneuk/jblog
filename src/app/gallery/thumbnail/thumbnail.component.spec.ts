15e83ca982b97bd82a094db5dd2cdfe62c9d25e3
Jason Browne <jason@browne.me.uk>
2017-06-30 16:50:55 +0100

    Add thumbnail component test

--- src/app/gallery/thumbnail/thumbnail.component.spec.ts
+++ src/app/gallery/thumbnail/thumbnail.component.spec.ts
@@ -1,14 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { GalleryFormatPipe } from '../gallery-format.pipe';
 
 import { ThumbnailComponent } from './thumbnail.component';
+
+const mockImageInfo = {
+  id: 1,
+  title: 'title',
+  date: Date.now(),
+  description: 'description',
+  thumbnail: './thumb.jpg',
+  src: './src.jpg',
+  containingAlbums: [{name: 'album', title: 'Album'}],
+  featured: false
+};
 
 describe('ThumbnailComponent', () => {
   let component: ThumbnailComponent;
   let fixture: ComponentFixture<ThumbnailComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ThumbnailComponent ]
+      imports: [ RouterTestingModule ],
+      declarations: [
+        GalleryFormatPipe,
+        ThumbnailComponent
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +35,15 @@ describe('ThumbnailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ThumbnailComponent);
     component = fixture.componentInstance;
+    component.data = mockImageInfo;
+    compiled = fixture.debugElement.nativeElement;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should render elements correctly', () => {
     expect(component).toBeTruthy();
+    expect(compiled.querySelector('.image-area img').src).toBe('./src.jpg');
+    expect(compiled.querySelector('.text-area .title').textContent.trim()).toBe('title');
+    expect(compiled.querySelector('.text-area .galleries').textContent.trim()).toBe('Album');
   });
 });
