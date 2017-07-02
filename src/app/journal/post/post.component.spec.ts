c28c6a65b93dcf8ce9c55daf350f4a534ae0c057
Jason Browne <jason@browne.me.uk>
2017-07-02 23:19:17 +0100

    Add some tests for post component

--- src/app/journal/post/post.component.spec.ts
+++ src/app/journal/post/post.component.spec.ts
@@ -1,14 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { TextParsingService } from '../../shared/text-parsing.service';
+import { MockTextParsingService } from '../../shared/mocks/mock-text-parsing.service';
 
 import { PostComponent } from './post.component';
 
+const mockPostData = {
+  postId: 1,
+  date: Date.now(),
+  title: 'post title',
+  content: 'long content here',
+  tags: ['one', 'two']
+};
+
 describe('PostComponent', () => {
+  const mockTextParsingService = new MockTextParsingService();
+
   let component: PostComponent;
   let fixture: ComponentFixture<PostComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PostComponent ]
+      imports: [ RouterTestingModule ],
+      declarations: [ PostComponent ],
+      providers: [
+        { provide: TextParsingService, useValue: mockTextParsingService }
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +35,16 @@ describe('PostComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PostComponent);
     component = fixture.componentInstance;
+    component.data = mockPostData;
+    compiled = fixture.debugElement.nativeElement;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render content correctly', () => {
+    expect(compiled.querySelector('article h1').textContent.trim()).toBe('post title');
+    expect(compiled.querySelector('.content-info-area ul li').textContent.trim()).toBe('Post 1');
+    expect(compiled.querySelector('.content-area').textContent.trim()).toBe('long content here was parsed');
+    // expect(compiled.querySelector('').textContent.trim()).toBe('');
+    expect(component.hasTags()).toBeTruthy();
   });
 });
