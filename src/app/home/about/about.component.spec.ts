ba5db329bedd7df34e6f3e53f449fa20556b66d3
Jason Browne <jason@browne.me.uk>
2017-07-02 22:34:32 +0100

    Fix about component test

--- src/app/home/about/about.component.spec.ts
+++ src/app/home/about/about.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AboutComponent } from './about.component';
 
@@ -8,6 +9,7 @@ describe('AboutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ RouterTestingModule ],
       declarations: [ AboutComponent ]
     })
     .compileComponents();
@@ -19,6 +21,8 @@ describe('AboutComponent', () => {
     fixture.detectChanges();
   });
 
+  // This is a static, content-only page and thus only
+  // needs a basic test
   it('should create', () => {
     expect(component).toBeTruthy();
   });
