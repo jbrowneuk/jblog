942f5c0ff98ad4c1f299580a0e9292c34ba7fbfe
Jason B <jason@browne.me.uk>
2017-08-11 15:01:45 +0100

    Update specs so that they test the correct things

--- src/app/journal/post/post.component.spec.ts
+++ src/app/journal/post/post.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { TextParsingService } from '../../shared/text-parsing.service';
-// import { MockTextParsingService } from '../../shared/mocks/mock-text-parsing.service';
+import { MockTextParsingService } from '../../shared/mocks/mock-text-parsing.service';
 
 import { PostComponent } from './post.component';
 
@@ -10,12 +10,12 @@ const mockPostData = {
   postId: 1,
   date: Date.now(),
   title: 'post title',
-  content: '<p>Example post content with an emoji :blush: . Yay!</p>',
+  content: '<p>Example post content with an emoji :smile: . Yay!</p>',
   tags: ['one', 'two']
 };
 
 describe('PostComponent', () => {
-  // const mockTextParsingService = new MockTextParsingService();
+  const mockTextParsingService = new MockTextParsingService();
 
   let component: PostComponent;
   let fixture: ComponentFixture<PostComponent>;
@@ -26,8 +26,7 @@ describe('PostComponent', () => {
       imports: [ RouterTestingModule ],
       declarations: [ PostComponent ],
       providers: [
-        // { provide: TextParsingService, useValue: mockTextParsingService }
-        TextParsingService
+        { provide: TextParsingService, useValue: mockTextParsingService }
       ]
     })
     .compileComponents();
@@ -44,10 +43,9 @@ describe('PostComponent', () => {
   it('should render content correctly', () => {
     expect(compiled.querySelector('article h1').textContent.trim()).toBe('post title');
     expect(compiled.querySelector('.content-info-area ul li').textContent.trim()).toBe('Post 1');
-    // expect(compiled.querySelector('.content-area').textContent.trim()).toBe('long content here');
     expect(component.hasTags()).toBeTruthy();
 
     const output = compiled.querySelector('.content-area').textContent.trim();
-    expect(output).toContain('<img class="emojione" alt="😊" title=":blush:" src="');
+    expect(output).toContain('Example post content with an emoji :smile: . Yay! was parsed');
   });
 });
