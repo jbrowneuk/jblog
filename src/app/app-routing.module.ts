b866922dbf5a012ba34aef9431b6881adab29605
Jason B <jaysunbee@gmail.com>
2018-01-08 22:17:30 +0700

    Add recipe module to app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { ErrorComponent } from './error/error.component';
 
 const appRoutes: Routes = [
-  { path: 'journal', loadChildren: 'app/journal/journal.module#JournalModule' },
-  { path: 'code', loadChildren: 'app/code/code.module#CodeModule' },
   { path: 'art', loadChildren: 'app/gallery/gallery.module#GalleryModule' },
+  { path: 'code', loadChildren: 'app/code/code.module#CodeModule' },
+  { path: 'journal', loadChildren: 'app/journal/journal.module#JournalModule' },
+  { path: 'recipes', loadChildren: 'app/recipes/recipes.module#RecipesModule' },
   { path: '**', component: ErrorComponent }
 ];
 
