da2201f75acbb57aea6a7c9f443fdc075542bbd3
Jason B <jaysunbee@gmail.com>
2018-01-28 17:18:42 +0000

    Fix missing routing implementation

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -7,16 +7,19 @@ import { SharedModule } from '../shared/shared.module';
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
 import { RecipeComponent } from './recipe/recipe.component';
 
-const journalRoutes: Routes = [
-  { path: '', redirectTo: 'recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipesListComponent },
-  { path: 'recipe', component: RecipeComponent },
-  { path: '**', redirectTo: 'recipes' }
+const sectionId = 3.3;
+const routes: Routes = [
+  { path: '', redirectTo: 'list', pathMatch: 'full' },
+  { path: 'list', component: RecipesListComponent, data: { sectionId: sectionId } },
+  { path: 'recipe', component: RecipeComponent, data: { sectionId: sectionId } },
+  { path: '**', redirectTo: 'list' }
 ];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    SharedModule
   ],
   declarations: [RecipesListComponent, RecipeComponent]
 })
