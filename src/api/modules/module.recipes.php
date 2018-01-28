988e69cb2b110b4f0984b65747c2867245e368d1
Jason B <jaysunbee@gmail.com>
2018-01-28 23:58:21 +0000

    Reduce amount of mock data and remove commented code

--- src/api/modules/module.recipes.php
+++ src/api/modules/module.recipes.php
@@ -17,12 +17,12 @@ class ApiModule {
   public function handleResponse() {
     $output = array();
 
-    for ($i = 0; $i < 16; $i++) {
+    for ($i = 0; $i < 8; $i++) {
       $food = array(
         "id"          => "food" . $i,
         "title"       => "Food ID " . $i,
         "description" => "WIP: still being coded here",
-        "imageSrc"    => "//picsum.photos/300/200/?image=" . ($i + 1)
+        "imageSrc"    => "//lorempixel.com/300/200/food/" . ($i + 1)
       );
 
       $output[] = $food;
