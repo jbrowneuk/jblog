e4999419187ff78bcd769649c50a68fa257dab82
Jason B <jaysunbee@gmail.com>
2018-04-08 00:50:46 +0100

    Implement feature toggle for new projects area

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -1,18 +1,28 @@
 import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, Route } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module';
 
 import { DisambiguationPageComponent } from './disambiguation-page/disambiguation-page.component';
+import { environment } from '../../environments/environment.prod';
 
 const sectionId = 3.0;
-const routes: Routes = [
-  { path: 'projects', component: DisambiguationPageComponent, pathMatch: 'full', data: { sectionId: sectionId } },
-  { path: 'projects/code', loadChildren: '../code/code.module#CodeModule' },
-  { path: 'projects/recipes', loadChildren: '../recipes/recipes.module#RecipesModule' },
-  { path: 'code', redirectTo: 'projects/code' }
-];
+
+let routes: Routes = [];
+if (environment.featureToggles.includes('newProjectStructure')) {
+  routes.push(...[
+    { path: 'projects', component: DisambiguationPageComponent, pathMatch: 'full', data: { sectionId: sectionId } },
+    { path: 'projects/code', loadChildren: '../code/code.module#CodeModule' },
+    { path: 'projects/recipes', loadChildren: '../recipes/recipes.module#RecipesModule' },
+    { path: 'code', redirectTo: 'projects/code' }
+  ]);
+} else {
+  routes.push(...[
+    { path: 'code', loadChildren: '../code/code.module#CodeModule' },
+    { path: 'projects', redirectTo: 'code' }
+  ]);
+}
 
 @NgModule({
   imports: [
