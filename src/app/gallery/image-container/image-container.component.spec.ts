f6973ea6c056546fb76dbd68ee25ee3a8548733e
Jason B <jaysunbee@gmail.com>
2018-04-08 19:52:31 +0100

    Update mock usage for image container component test

--- src/app/gallery/image-container/image-container.component.spec.ts
+++ src/app/gallery/image-container/image-container.component.spec.ts
@@ -2,21 +2,34 @@ import { SimpleChange, SimpleChanges } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpModule } from '@angular/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs/Observable';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { It, Mock, Times } from 'typemoq';
 
 import { GalleryFormatPipe } from '../gallery-format.pipe';
-import { ThumbnailComponent } from '../thumbnail/thumbnail.component';
 import { ImageService } from '../image.service';
-import { MockImageService } from '../mocks/mock-image.service';
 import { LoadSpinnerComponent } from '../../shared/load-spinner/load-spinner.component';
+
+import { MOCK_IMAGEDATA } from '../mocks/mock-data';
 
 import { ImageContainerComponent } from './image-container.component';
 
 describe('ImageContainerComponent', () => {
-  const mockImageService = new MockImageService();
+  const mockImageService = Mock.ofType<ImageService>();
+
   let component: ImageContainerComponent;
   let fixture: ComponentFixture<ImageContainerComponent>;
 
-  beforeEach(async(() => {
+  const setupImagesFromAlbumCall = () => {
+    mockImageService.setup(s => s.getImagesFromAlbum(It.isAnyString(), It.isAnyNumber(), It.isAnyNumber()))
+      .returns(() => Observable.of([MOCK_IMAGEDATA]));
+  };
+
+  beforeEach(() => {
+    mockImageService.reset();
+    setupImagesFromAlbumCall();
+
     TestBed.configureTestingModule({
       imports: [
         HttpModule,
@@ -25,48 +38,39 @@ describe('ImageContainerComponent', () => {
       declarations: [
         ImageContainerComponent,
         GalleryFormatPipe,
-        ThumbnailComponent,
         LoadSpinnerComponent
       ],
       providers: [
-        {provide: ImageService, useValue: mockImageService}
-      ]
+        { provide: ImageService, useFactory: () => mockImageService.object }
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(ImageContainerComponent);
     component = fixture.componentInstance;
+    component.page = 1;
+    component.albumName = 'default';
+
     fixture.detectChanges();
   });
 
-  it('should update images when page changed', async(() => {
-    spyOn(mockImageService, 'getImagesFromAlbum').and.callThrough();
-    expect(mockImageService.getImagesFromAlbum).not.toHaveBeenCalled();
+  it('should update images when page changed', () => {
+    const newPage = 2;
 
-    component.page = 2;
-    component.ngOnChanges({page: new SimpleChange(1, 2, true)});
+    component.page = newPage;
+    component.ngOnChanges({page: new SimpleChange(1, newPage, true)});
 
-    fixture.detectChanges();
-    fixture.whenStable().then(() => {
-      fixture.detectChanges();
-      expect(mockImageService.getImagesFromAlbum).toHaveBeenCalled();
-    });
-  }));
+    mockImageService.verify(x => x.getImagesFromAlbum(It.isAnyString(), It.isValue(newPage), It.isAnyNumber()), Times.once());
+  });
 
-  it('should update images when album changed', async(() => {
-    spyOn(mockImageService, 'getImagesFromAlbum').and.callThrough();
-    expect(mockImageService.getImagesFromAlbum).not.toHaveBeenCalled();
+  it('should update images when album changed', () => {
+    const newAlbumName = 'anotherOneHere';
 
-    component.albumName = 'another';
-    component.ngOnChanges({albumName: new SimpleChange('_default', 'albumName', true)});
+    component.albumName = newAlbumName;
+    component.ngOnChanges({albumName: new SimpleChange('_default', newAlbumName, true)});
 
-    fixture.detectChanges();
-    fixture.whenStable().then(() => {
-      fixture.detectChanges();
-      expect(mockImageService.getImagesFromAlbum).toHaveBeenCalled();
-    });
-  }));
+    mockImageService.verify(x => x.getImagesFromAlbum(It.isValue(newAlbumName), It.isAnyNumber(), It.isAnyNumber()), Times.once());
+  });
 
 });
