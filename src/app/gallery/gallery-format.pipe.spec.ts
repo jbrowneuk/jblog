77e98396bc486119fa5b57aae1f5ed525ba9de6a
Jason B <jaysunbee@gmail.com>
2018-04-15 17:20:35 +0100

    Add tests for gallery formatting pipe

--- src/app/gallery/gallery-format.pipe.spec.ts
+++ src/app/gallery/gallery-format.pipe.spec.ts
@@ -5,4 +5,24 @@ describe('GalleryFormatPipe', () => {
     const pipe = new GalleryFormatPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should concatenate album titles', () => {
+    const albumInfo = [
+      { title: 'one', name: '' },
+      { title: 'two', name: '' },
+      { title: 'three', name: '' }
+    ];
+
+    const pipe = new GalleryFormatPipe();
+    const actualOutput = pipe.transform(albumInfo);
+
+    expect(actualOutput).toBe('one, two, three');
+  });
+
+  it('should return ‘None’ if the information array is empty', () => {
+    const pipe = new GalleryFormatPipe();
+    const actualOutput = pipe.transform([]);
+
+    expect(actualOutput).toBe('None');
+  });
 });
