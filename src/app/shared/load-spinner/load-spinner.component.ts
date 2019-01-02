b6623eea00869ebcc3eeb3f3ceb9b3fed7df84ab
Jason B <jaysunbee@gmail.com>
2019-01-02 10:47:26 +0700

    Update load spinner to use new colour palette

--- src/app/shared/load-spinner/load-spinner.component.ts
+++ src/app/shared/load-spinner/load-spinner.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'jblog-load-spinner',
   template: `
-    <div class="load-spinner"><div></div></div>
+    <div class="load-spinner">
+      <div class="border"></div>
+      <div class="logo">
+        <svg><use xlink:href="#sitesheet-logo" /></svg>
+      </div>
+    </div>
   `
 })
 export class LoadSpinnerComponent {}
