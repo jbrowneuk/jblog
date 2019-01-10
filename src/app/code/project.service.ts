65197cf9007996caba9823f6a1fe89e9d2863c7f
Jason B <jaysunbee@gmail.com>
2019-01-10 16:08:04 +0700

    Update projects service to use new backend API

--- src/app/code/project.service.ts
+++ src/app/code/project.service.ts
@@ -1,10 +1,13 @@
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, Optional } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { BASE_PATH } from '../variables';
 import { Project } from './project';
+
+const API_URL = '/?projects';
 
 /**
  * A service which handles requesting projects and their details from an API
@@ -12,11 +15,24 @@ import { Project } from './project';
  */
 @Injectable()
 export class ProjectService {
+  /**
+   * The fallback base URL to use if one is not provided by the environment.
+   */
+  protected basePath = 'http://localhost/api';
 
   /**
    * Injecting constructor.
    */
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    @Optional()
+    @Inject(BASE_PATH)
+    basePath: string
+  ) {
+    if (basePath) {
+      this.basePath = basePath;
+    }
+  }
 
   /**
    * Calls out to the backend and gets a list of projects.
@@ -24,13 +40,16 @@ export class ProjectService {
    * @param pageNumber {number} the page number to load.
    * @param amount {number} optional number of projects to load.
    */
-  public getProjects(pageNumber: number, amount: number = 0): Observable<Project[]> {
+  public getProjects(
+    pageNumber: number,
+    amount: number = 0
+  ): Observable<Project[]> {
     if (amount <= 0) {
       amount = 8;
     }
 
-    return this.http.get<Project[]>('/assets/mock-data/projects.json')
+    return this.http
+      .get<Project[]>(`${this.basePath}${API_URL}`)
       .pipe(map(project => project.slice(0, amount)));
   }
-
 }
