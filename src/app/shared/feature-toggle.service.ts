950e72b7140c7ba42bb4f8d4f1dac2ff437a1ad1
Jason B <jaysunbee@gmail.com>
2019-02-12 23:46:57 +0000

    Remove unused feature toggle service

--- src/app/shared/feature-toggle.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
-
-@Injectable()
-export class FeatureToggleService {
-  public isEnabled(key: string): boolean {
-    return environment.featureToggles.includes(key);
-  }
-}
