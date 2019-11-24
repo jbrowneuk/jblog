587a7ba6e380a346518ac21e97e6e377a57ad3e1
Jason B <jason@browne.me.uk>
2019-11-24 17:28:56 +0000

    Refactor colour variables names to more accurately reflect usage

--- src/app/shared/formatted-text/formatted-text.component.ts
+++ src/app/shared/formatted-text/formatted-text.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'jblog-text',
   template: `
-    <ngx-md><ng-content></ng-content></ngx-md>
-  `
+    <ngx-md class="text-area"><ng-content></ng-content></ngx-md>
+  `,
+  styleUrls: ['./formatted-text.component.scss']
 })
 export class FormattedTextComponent {}
