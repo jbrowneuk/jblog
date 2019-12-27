6a56b0f1da4d4cf1bcc8a563860a66cfea0c9830
Jason B <jason@browne.me.uk>
2019-12-27 23:20:03 +0000

    Implement initial pass of admin page post list

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,18 +4,30 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AuthenticationGuard } from '../authentication.guard';
 import { AdminComponent } from './admin/admin.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 const adminRoutes: Routes = [
   {
     path: '',
     component: AdminComponent,
     canActivate: [AuthenticationGuard],
-    children: []
+    children: [
+      {
+        path: 'posts',
+        canActivateChild: [AuthenticationGuard],
+        children: [{ path: '', component: PostListComponent }]
+      },
+      {
+        path: '',
+        redirectTo: 'posts',
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, PostListComponent],
   imports: [CommonModule, RouterModule.forChild(adminRoutes)]
 })
 export class AdminModule {}
