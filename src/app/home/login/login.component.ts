f5d9b6159116ac4222bbb2586610cf02bab3fd2f
Jason B <jason@browne.me.uk>
2019-12-28 22:27:27 +0000

    Improve login redirection behaviour

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,7 +1,7 @@
-import { skip } from 'rxjs/operators';
+import { combineLatest } from 'rxjs';
 
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { UserService } from '../../services/user.service';
 
@@ -10,13 +10,35 @@ import { UserService } from '../../services/user.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   public username: string;
   public password: string;
   public loginError: boolean;
 
-  constructor(private userService: UserService, private router: Router) {
+  private returnTo: string;
+
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
     this.loginError = false;
+  }
+
+  public ngOnInit() {
+    // Route to given URL if provided
+    combineLatest([
+      this.userService.authenticatedUser$,
+      this.route.queryParams
+    ]).subscribe({
+      next: ([user, params]) => {
+        this.returnTo = params.returnTo;
+
+        if (user && this.returnTo) {
+          this.router.navigate(['/', this.returnTo]);
+        }
+      }
+    });
   }
 
   public submitForm(evt: Event): void {
@@ -26,24 +48,25 @@ export class LoginComponent {
     this.loginError = false;
 
     this.userService.initialiseSession(this.username, this.password).subscribe({
-      error: () => (this.loginError = true),
-      next: this.fetchUserInfo.bind(this)
+      next: this.fetchUserInfo.bind(this),
+      error: () => (this.loginError = true)
     });
   }
 
   private fetchUserInfo(): void {
     // Initial subscribed value is previous; skip
-    this.userService
-      .fetchUser()
-      .pipe(skip(1))
-      .subscribe({
-        next: this.onLoginComplete.bind(this),
-        error: err => console.error(err)
-      });
+    this.userService.fetchUser().subscribe({
+      next: this.onLoginComplete.bind(this),
+      error: () => (this.loginError = true)
+    });
   }
 
   private onLoginComplete(user: any): void {
-    const route = user ? '/admin' : '/';
-    this.router.navigate([route]);
+    if (!user) {
+      return;
+    }
+
+    const route = this.returnTo || 'admin';
+    this.router.navigate(['/', route]);
   }
 }
