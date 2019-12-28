537f315eab1c3477a5fe0ce643cd215015b738c6
Jason B <jason@browne.me.uk>
2019-12-28 15:26:51 +0000

    Implement redirect to login in authentication guard

--- src/app/authentication.guard.spec.ts
+++ src/app/authentication.guard.spec.ts
@@ -1,21 +1,25 @@
-import { IMock, Mock } from 'typemoq';
+import { IMock, It, Mock, Times } from 'typemoq';
 
-import { async, inject, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
 import { AuthenticationGuard } from './authentication.guard';
 import { UserService } from './services/user.service';
 
 describe('AuthenticationGuard', () => {
   let mockUserService: IMock<UserService>;
+  let mockRouter: IMock<Router>;
   let guard: AuthenticationGuard;
 
   beforeEach(() => {
     mockUserService = Mock.ofType<UserService>();
+    mockRouter = Mock.ofType<Router>();
 
     TestBed.configureTestingModule({
       providers: [
         AuthenticationGuard,
-        { provide: UserService, useFactory: () => mockUserService.object }
+        { provide: UserService, useFactory: () => mockUserService.object },
+        { provide: Router, useFactory: () => mockRouter.object }
       ]
     });
 
@@ -26,14 +30,16 @@ describe('AuthenticationGuard', () => {
     expect(guard).toBeTruthy();
   });
 
-  const testedMethods = ['canActivate', 'canActivateChild', 'canLoad'];
+  const testedMethods = ['canActivate', 'canActivateChild'];
 
   testedMethods.forEach(method => {
     describe(method, () => {
+      const mockState = { url: 'mock-url' };
+
       it('should return true if user is logged in', () => {
         mockUserService.setup(s => s.isLoggedIn).returns(() => true);
 
-        const actual = (guard[method] as Function).call(guard, null, null);
+        const actual = (guard[method] as Function).call(guard, null, mockState);
 
         expect(actual).toBeTruthy();
       });
@@ -41,10 +47,56 @@ describe('AuthenticationGuard', () => {
       it('should return false if user is not logged in', () => {
         mockUserService.setup(s => s.isLoggedIn).returns(() => false);
 
-        const actual = (guard[method] as Function).call(guard, null, null);
+        const actual = (guard[method] as Function).call(guard, null, mockState);
 
         expect(actual).toBeFalsy();
       });
+
+      it('should redirect if user is not logged in', () => {
+        mockUserService.setup(s => s.isLoggedIn).returns(() => false);
+
+        (guard[method] as Function).call(guard, null, mockState);
+
+        mockRouter.verify(
+          r => r.navigate(It.isValue(['/login']), It.isAny()),
+          Times.once()
+        );
+
+        expect().nothing();
+      });
+    });
+  });
+
+  describe('canLoad', () => {
+    const mockRoute = { path: 'mock-url' };
+
+    it('should return true if user is logged in', () => {
+      mockUserService.setup(s => s.isLoggedIn).returns(() => true);
+
+      const actual = guard.canLoad(mockRoute, null);
+
+      expect(actual).toBeTruthy();
+    });
+
+    it('should return false if user is not logged in', () => {
+      mockUserService.setup(s => s.isLoggedIn).returns(() => false);
+
+      const actual = guard.canLoad(mockRoute, null);
+
+      expect(actual).toBeFalsy();
+    });
+
+    it('should redirect if user is not logged in', () => {
+      mockUserService.setup(s => s.isLoggedIn).returns(() => false);
+
+      guard.canLoad(mockRoute, null);
+
+      mockRouter.verify(
+        r => r.navigate(It.isValue(['/login']), It.isAny()),
+        Times.once()
+      );
+
+      expect().nothing();
     });
   });
 });
--- src/app/authentication.guard.ts
+++ src/app/authentication.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {
-    ActivatedRouteSnapshot, CanActivate, CanActivateChild, CanLoad, Route, RouterStateSnapshot,
-    UrlSegment
+    ActivatedRouteSnapshot, CanActivate, CanActivateChild, CanLoad, NavigationExtras, Route, Router,
+    RouterStateSnapshot, UrlSegment
 } from '@angular/router';
 
 import { UserService } from './services/user.service';
@@ -11,23 +11,34 @@ import { UserService } from './services/user.service';
 })
 export class AuthenticationGuard
   implements CanActivate, CanActivateChild, CanLoad {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean {
-    return this.userService.isLoggedIn;
+    return this.checkLogin(state.url);
   }
 
   canActivateChild(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean {
-    return this.userService.isLoggedIn;
+    return this.checkLogin(state.url);
   }
 
   canLoad(route: Route, segments: UrlSegment[]): boolean {
-    return this.userService.isLoggedIn;
+    return this.checkLogin(route.path);
+  }
+
+  private checkLogin(url?: string): boolean {
+    if (this.userService.isLoggedIn) {
+      return true;
+    }
+
+    const navigationOpts = url ? { queryParams: { returnTo: url } } : undefined;
+
+    this.router.navigate(['/login'], navigationOpts);
+    return false;
   }
 }
