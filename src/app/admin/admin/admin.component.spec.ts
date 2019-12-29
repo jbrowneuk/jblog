c0fd3bad1dd291df732a6a5d74bcda392375ff97
Jason B <jason@browne.me.uk>
2019-12-29 00:19:59 +0000

    Fix title not changing for admin panel

--- src/app/admin/admin/admin.component.spec.ts
+++ src/app/admin/admin/admin.component.spec.ts
@@ -1,16 +1,25 @@
+import { IMock, It, Mock, Times } from 'typemoq';
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import { TitleService } from '../../shared/title.service';
 import { AdminComponent } from './admin.component';
 
 describe('AdminComponent', () => {
+  let mockTitleService: IMock<TitleService>;
   let component: AdminComponent;
   let fixture: ComponentFixture<AdminComponent>;
 
   beforeEach(async(() => {
+    mockTitleService = Mock.ofType<TitleService>();
+
     TestBed.configureTestingModule({
       declarations: [AdminComponent],
-      imports: [RouterTestingModule]
+      imports: [RouterTestingModule],
+      providers: [
+        { provide: TitleService, useFactory: () => mockTitleService.object }
+      ]
     })
       .compileComponents()
       .then(() => {
@@ -22,5 +31,10 @@ describe('AdminComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set title', () => {
+    mockTitleService.verify(x => x.setTitle(It.isValue('Admin')), Times.once());
+    expect().nothing();
   });
 });
--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { TitleService } from '../../shared/title.service';
 
 @Component({
   selector: 'jblog-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent {}
+export class AdminComponent implements OnInit {
+  constructor(private titleService: TitleService) {}
+
+  ngOnInit() {
+    this.titleService.setTitle('Admin');
+  }
+}
