6849c72b20a4d810cdcedc5ff7e61c9bf42fad83
Jason B <jason@browne.me.uk>
2019-12-29 13:31:40 +0000

    Fix insights page visuals

--- src/app/gallery/insights/insights.component.ts
+++ src/app/gallery/insights/insights.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'jblog-insights',
-  templateUrl: './insights.component.html'
+  templateUrl: './insights.component.html',
+  styleUrls: ['./insights.component.scss']
 })
 export class InsightsComponent {}
