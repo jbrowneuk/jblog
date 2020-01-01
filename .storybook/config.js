427fc29adfab78d06919dc5d9c232324a47de19d
Jason B <jason@browne.me.uk>
2020-01-01 19:52:20 +0000

    Sort stories alphabetically

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,15 @@
-import { configure } from '@storybook/angular';
+import { addParameters, configure } from '@storybook/angular';
 
 function loadStories() {
   // automatically import all files ending in *.stories.ts
   const req = require.context('../src/app', true, /.stories.ts$/);
   req.keys().forEach(filename => req(filename));
 }
+
+addParameters({
+  options: {
+    storySort: (a, b) => a[1].id.localeCompare(b[1].id)
+  }
+});
 
 configure(loadStories, module);
