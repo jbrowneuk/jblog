57be38908786e2e1a4c6fd0c60abb184bd316f0a
Jason B <jason@browne.me.uk>
2020-01-02 00:27:46 +0000

    Apply crossfade animations to admin routes

--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 import { TitleService } from '../../shared/title.service';
+import { ROUTE_TRANSITIONS } from './admin.route-animations';
 
 @Component({
   selector: 'jblog-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
+  animations: [ROUTE_TRANSITIONS]
 })
 export class AdminComponent implements OnInit {
   constructor(private titleService: TitleService) {}
---
+++ src/app/admin/admin/admin.route-animations.ts
@@ -0,0 +1,40 @@
+import {
+    animate, animateChild, AnimationQueryOptions, group, query, style, transition, trigger
+} from '@angular/animations';
+
+const animationEasing = '200ms ease-out';
+const optionalAnimation: AnimationQueryOptions = { optional: true };
+
+const enterLeaveQuery = query(
+  ':enter, :leave',
+  style({ position: 'absolute', left: 0, right: 0 }),
+  optionalAnimation
+);
+
+const fadeRoutes = [
+  group([
+    enterLeaveQuery,
+    query(
+      ':enter',
+      [
+        style({ opacity: 0 }),
+        animate(animationEasing, style({ opacity: 1 })),
+        animateChild()
+      ],
+      optionalAnimation
+    ),
+    query(
+      ':leave',
+      [
+        style({ opacity: 1 }),
+        animate(animationEasing, style({ opacity: 0 })),
+        animateChild()
+      ],
+      optionalAnimation
+    )
+  ])
+];
+
+export const ROUTE_TRANSITIONS = trigger('adminRouterAnimations', [
+  transition('* => *', fadeRoutes)
+]);
