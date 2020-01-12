fa61c7631cea45da8dac93a75f8fadfc60ed6886
Jason B <jason@browne.me.uk>
2020-01-12 23:39:41 +0000

    Implement first pass of post modification from frontend

---
+++ src/app/admin/post-admin.service.ts
@@ -0,0 +1,33 @@
+import { Observable } from 'rxjs';
+
+import { Inject, Injectable, Optional } from '@angular/core';
+
+import { PostData } from '../model/post-data';
+import { RestService } from '../services/rest.service';
+import { UserService } from '../services/user.service';
+import { BASE_PATH } from '../variables';
+
+const API_URL = '/?admin&action=posts';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostAdminService {
+  protected basePath = 'http://localhost/api';
+
+  constructor(
+    private userService: UserService,
+    @Optional() @Inject(BASE_PATH) basePath: string
+  ) {
+    if (basePath) {
+      this.basePath = basePath;
+    }
+  }
+
+  public sendPost(post: PostData): Observable<any> {
+    const body = new FormData();
+    body.append('data', JSON.stringify(post));
+
+    return this.userService.authPost(`${this.basePath}${API_URL}`, body);
+  }
+}
