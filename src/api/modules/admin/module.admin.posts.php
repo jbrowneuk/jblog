21fcb79f7b3c4b374a54526bd03d4823553b0097
Jason B <jason@browne.me.uk>
2020-01-12 23:58:29 +0000

    Implement first pass of post edit API

--- src/api/modules/admin/module.admin.posts.php
+++ src/api/modules/admin/module.admin.posts.php
@@ -33,14 +33,15 @@ class AdminModule {
 
     $id = $rawPostData->postId;
     if (!isset($id) || $id < 0) {
-      // Create post
-      $this->createPost($rawPostData);
-    } else {
-      // Edit post
-      print "EDIT AN OLD POST\n\n" . print_r($rawPostData, true);
+      return $this->createPost($rawPostData);
     }
+
+    $this->editPost($rawPostData);
   }
 
+  //============================================================================
+  // Creates a new post
+  //============================================================================
   private function createPost($postData) {
     $fields = array(
       POST_ID_INDEX => null,
@@ -65,4 +66,39 @@ class AdminModule {
     ResponseHelpers::outputWithPlainTextHeader("Created a post");
   }
 
+  //============================================================================
+  // Edits an existing post
+  //============================================================================
+  private function editPost($postData) {
+    // TODO: handle slug change
+
+    $fields = array(
+      POST_ID_INDEX => $postData->postId,
+      POST_TITLE_INDEX => $postData->title,
+      POST_CONTENT_INDEX => $postData->content,
+      POST_DATE_INDEX => $postData->date,
+      POST_STATUS_INDEX => $postData->status,
+      POST_SLUG_INDEX => $postData->slug,
+      POST_TAG_INDEX => implode(' ', $postData->tags),
+      POST_MODIFICATIONDATE_INDEX => time(),
+      POST_COMMENT_STATUS_INDEX => 'open', // todo
+      POST_COMMENT_COUNT_INDEX => 0 // todo
+    );
+
+    $where = array(
+      POST_ID_INDEX => $postData->postId,
+      POST_SLUG_INDEX => $postData->slug
+    );
+
+    $options = array(); // todo: update the impl
+
+    $success = $this->db->update($fields, $where, $options, TABLE_SCOPE);
+    if (!$success) {
+      // TODO: respond with error JSON and some other type of HTTP code
+      print "FAIL" . print_r($this->db->getErrorMessages(), true);
+      return ResponseHelpers::respondServerError();
+    }
+
+    ResponseHelpers::outputWithPlainTextHeader("Edited a post");
+  }
 }
