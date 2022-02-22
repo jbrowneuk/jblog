5a606e6118996139a93957fd2827b1b630c87c58
Jason B <jason@browne.me.uk>
2022-02-22 14:44:05 +0000

    Bump angular version to 13.2

--- karma.conf.js
+++ karma.conf.js
@@ -1,47 +1,44 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-// Set the following to true to automatically start Google Chrome for testing
-// If set to false, you will need to manually open localhost:9876 to run the tests
-const autostartChrome = false;
-
-// Test runner plugins
-const plugins = [
-  require('karma-jasmine'),
-  require('karma-jasmine-html-reporter'),
-  require('karma-coverage-istanbul-reporter'),
-  require('@angular-devkit/build-angular/plugins/karma')
-];
-
-let browsers = undefined;
-
-if (autostartChrome) {
-  plugins.push(require('karma-chrome-launcher'));
-  browsers = ['Chrome'];
-}
-
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins,
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
     client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    mime: {
-      'text/x-typescript': ['ts', 'tsx']
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/jblog'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers,
-    singleRun: false
+    browsers: ['Chrome'],
+    singleRun: false,
+    restartOnFileChange: true
   });
 };
